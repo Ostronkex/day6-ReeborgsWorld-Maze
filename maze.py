fb95669d3367c281224833ee709907be660643f3
Erik Magnusson <erikmagnusson.privat@gmail.com>
2024-07-26 13:27:03 +0200

    Create maze.py

---
+++ maze.py
@@ -0,0 +1,19 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left() 
+
+def keep_going():
+    while at_goal() != True:
+        if wall_in_front() == True:
+            turn_left()
+        while wall_on_right() == True and wall_in_front() != True:
+            move()
+        while front_is_clear():
+            move()
+        if right_is_clear() == True and at_goal() != True:
+            turn_right()
+            move()
+            turn_right()
+
+keep_going()
